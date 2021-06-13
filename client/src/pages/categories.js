cf822f6f43fc8c42d98880f792abfa0f6667b1a6
vmidor <76964172+vmidor@users.noreply.github.com>
2021-06-13 13:54:54 -0400

    home page in working

---
+++ client/src/pages/categories.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+
+// export default class Categories extends React.Component {
+//     render() {
+//         return (
+
+//             Categories.propTypes = {
+//                 fadeInLeft: PropTypes.string,
+//                 shake: PropTypes.string
+//             };
