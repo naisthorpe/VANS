10df275545cfc297349d9242f6dd176214710cea
vmidor <76964172+vmidor@users.noreply.github.com>
2021-06-15 22:32:13 -0400

    Categories added and upcoming and creacre pages are fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 import { isNil } from 'lodash';
 import React, { useEffect } from 'react';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/home';
 import Upcoming from './pages/Upcoming';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/login';
 import Signup from './pages/signUp';
+import Categories from './components/Categories';
+import Footer from './components/Footer';
 import { LOADING, SET_USER, UNSET_USER } from './store/actions';
 import { useStoreContext } from './store/store';
+import MenuItem from "./components/MenuItem";
+import Sidemenu from "./components/Sidemenu";
+// import { Typography } from '@material-ui/core';
 
 const App = () => {
   const history = useHistory();
@@ -32,6 +37,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      {/* <Typography /> */}
 
       {state.user ? (
         <Switch>
@@ -46,7 +52,12 @@ const App = () => {
             <Route exact path="/signup" component={Signup} />
             <Redirect to="/login" />
           </Switch>
+
+
         )}
+      <Categories />
+      <MenuItem />
+      <Footer />
     </div>
   );
 };
