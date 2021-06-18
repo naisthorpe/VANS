31349c039481a5311920ddf93213bb0bde692606
vmidor <76964172+vmidor@users.noreply.github.com>
2021-06-18 14:40:55 -0400

    category tab is done, next step footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,13 @@ import { isNil } from 'lodash';
 import React, { useEffect } from 'react';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/home';
+import Home from '../src/pages/Home';
 import Login from './pages/login';
 import Signup from './pages/signUp';
-import Categories from './components/Categories';
-import Footer from './components/Footer';
 import { LOADING, SET_USER, UNSET_USER } from './store/actions';
 import { useStoreContext } from './store/store';
 import TwilioTest from "./pages/TwilioTest";
 
-import MenuItem from "./components/MenuItem";
-import Sidemenu from "./components/Sidemenu";
-// import { Typography } from '@material-ui/core';
 
 const App = () => {
   const history = useHistory();
@@ -43,28 +38,16 @@ const App = () => {
           <Route exact path="/twilio" component={TwilioTest} />
         </Switch>
       ) : (
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/twilio" component={TwilioTest} />
-          <Redirect to="/login" />
-        </Switch>
-      )}
-
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
+            <Route exact path="/twilio" component={TwilioTest} />
             <Redirect to="/login" />
           </Switch>
-
-
         )}
-      <Categories />
-      <MenuItem />
-      <Footer />
-    </div>
+    </div >
+
   );
 };
 
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,9 @@
 import React from "react";
-class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer" className="container-fluid text-center py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-sm-12 col-md-8 mx-auto">
-                            Footer
-                        </div>
-                    </div>
 
-                </div>
-            </div>
-        );
-    }
-}
+const Footer = () => (
+    <div className="footer">
+        <p>This is some content in sticky footer</p>
+    </div>
+);
 
 export default Footer;
